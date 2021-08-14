fd842b47ad3e09895ebc34eff6f095a6c3ff7e5e
Nikola Paunovic <nikolapaunovic99@gmail.com>
2021-08-14 16:32:30 +0200

    Sredjivanje izgleda charta

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,7 +11,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('\$${spendintAmount.toStringAsFixed(0)}'),
+        FittedBox(
+          child: Text('\$${spendintAmount.toStringAsFixed(0)}'),
+        ),
         SizedBox(
           height: 10,
         ),
