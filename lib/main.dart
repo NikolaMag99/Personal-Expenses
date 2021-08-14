079c94b5bab25ac522d795a3d5bad4a071b70433
Nikola Paunovic <nikolapaunovic99@gmail.com>
2021-08-14 17:18:55 +0200

    Dodato dodavanje datuma pri dodavanju transakcija

--- lib/main.dart
+++ lib/main.dart
@@ -63,12 +63,12 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTransaction(String title, double amount) {
+  void _addNewTransaction(String title, double amount, DateTime chosenDate) {
     final newTx = Transaction(
       id: DateTime.now().toString(),
       title: title,
       amount: amount,
-      date: DateTime.now(),
+      date: chosenDate,
     );
 
     setState(() {
--- lib/widgets/newTransaction.dart
+++ lib/widgets/newTransaction.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -12,24 +13,46 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
 
-  final priceController = TextEditingController();
+  final _priceController = TextEditingController();
 
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final enteredPrice = double.parse(priceController.text);
+  DateTime? _selecetedDate;
 
-    if (enteredTitle.isEmpty || enteredPrice <= 0) {
+  void _submitData() {
+    if (_priceController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredPrice = double.parse(_priceController.text);
+
+    if (enteredTitle.isEmpty || enteredPrice <= 0 || _selecetedDate == null) {
       return;
     }
 
     widget.addTx(
       enteredTitle,
       enteredPrice,
+      _selecetedDate,
     );
 
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2021),
+      lastDate: DateTime.now(),
+    ).then((value) {
+      if (value == null) {
+        return;
+      }
+      setState(() {
+        _selecetedDate = value;
+      });
+    });
   }
 
   @override
@@ -43,28 +66,52 @@ class _NewTransactionState extends State<NewTransaction> {
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              onSubmitted: (_) => submitData(),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
               // onChanged: (val) {
               //   titleInput = val;
               // },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Price'),
-              controller: priceController,
+              controller: _priceController,
               keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData(),
+              onSubmitted: (_) => _submitData(),
               // onChanged: (val) {
               //   priceInput = val;
               // },
             ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selecetedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMMMd().format(_selecetedDate!)}',
+                    ),
+                  ),
+                  // ignore: deprecated_member_use
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date!',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _presentDatePicker,
+                  ),
+                ],
+              ),
+            ),
             // ignore: deprecated_member_use
-            FlatButton(
-              onPressed: submitData,
+            RaisedButton(
+              onPressed: _submitData,
               child: Text(
                 'Add Transaction',
               ),
-              textColor: Colors.purple,
+              color: Colors.purple,
+              textColor: Colors.white,
             )
           ],
         ),
